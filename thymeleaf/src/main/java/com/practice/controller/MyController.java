e0029e56909da0c83d5288ee4d95fff1e0bc9e18
ambuj-dubey <shriniwasambuj8055@gmail.com>
2024-05-10 00:28:25 +0530

    SpringBoot with Thymeleaf

---
+++ thymeleaf/src/main/java/com/practice/controller/MyController.java
@@ -0,0 +1,42 @@
+package com.practice.controller;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping(value ="/about", method = RequestMethod.GET)
+	public String about(Model model) {
+		System.out.println("Inside about handler");
+		
+		//Putting data in model
+		model.addAttribute("name","Ambuj Dubey");
+		model.addAttribute("CurrentDate", new Date(0).toLocaleString());
+		return "about"; //about.html it get called dynamically
+	}
+	
+	@GetMapping("/example-loop")
+	public String  iterateHandler(Model m) {								//Model is used to send the list 
+		
+		//create list, set collection
+		
+		List<String> names = List.of("Ambuj","Ankit","Aman","Nitya");
+		m.addAttribute("names",names);
+		return "iterate";
+	}
+	
+	@GetMapping("/condition")
+	public String ConditionalHandler(Model m) {
+		System.out.println("Conditional handler executed");
+		m.addAttribute("isActive", true);
+		return "condition";
+	}
+
+}
